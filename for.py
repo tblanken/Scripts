adaddd52f89c29da4b475a25ebdd5b56196401d9
Tim Blankenship <tim.blakenship@hp.com>
2017-07-25 14:49:18 -0700

    separated code examples into individual files

---
+++ for.py
@@ -0,0 +1,10 @@
+
+# For statement
+
+n = 101
+sum = 0
+
+for counter in range(1,n):
+  sum = sum + 1
+
+print("Sum of 1 until %d: %d" % (n,sum))
---
+++ hello.py
@@ -0,0 +1,6 @@
+
+print ('Hello world!')
+
+name = 'Monty'
+print('Hello, %s' % name)
+print('Hello, {}'.format(name))
