1c7c984d12b941cfd36d92f06759038354c4ffad
Tim Blankenship <tim.blakenship@hp.com>
2017-07-27 13:47:57 -0700

    added two while loops to make sure each player provides a selection between 1 and 5

--- py/rock-paper-scissors.py
+++ py/rock-paper-scissors.py
@@ -1,13 +1,13 @@
 #### Rock crushes scissors
 #### Rock crushes lizard
 #### Paper covers rock
-### Paper disproves Spock
-### Scissors cuts paper
-### Scisors decapitate lizard
-### Lizard poisons Spock
-### Lizard eats paper
+#### Paper disproves Spock
+#### Scissors cuts paper
+#### Scisors decapitate lizard
+#### Lizard poisons Spock
+#### Lizard eats paper
 #### Spock vaporizes rock
-### Spock smashes scissors 
+#### Spock smashes scissors 
 
 
 rock = 'rock'
@@ -19,46 +19,62 @@ spock = 'Spock'
 print "Welcome to the Rock, paper, Scissors, Lizard, Spock game"
 print""
 
-answer1 = input ("Player 1 please type 1 for rock, 2 for paper, 3 for scissors, 4 for lizard, 5 for Spock ")
-answer2 = input ("Player 2 please type 1 for rock, 2 for paper,  3 for scissors, 4 for lizard, 5 for Spock ")
+answer1 = None
+answer2 = None
 
-if answer1 == 1 and answer2 == 2:
-   print "Player 2 wins because paper covers rock! "
-elif answer1 == 1 and answer2 == 3:
-   print "Player 1 wins because rock crushes scissors! " 
-elif answer1 == 1 and answer2 == 4:
-   print "Player 1 wins because rock crushes lizard! "
-elif answer1 == 1 and answer2 == 5:
-   print "Player 2 wins becuase Spock vaporizes rock! "
-elif answer1 == 2 and answer2 == 1:
-   print "Player 2 wins because paper covers rock! "
-elif answer1 == 2 and answer2 == 3:
-   print "Player 2 wins because scissors cuts paper! "
-elif answer1 == 2 and answer2 == 4:
-   print "Player 2 wins because lizard eats paper! "
-elif answer1 == 2 and answer2 == 5:
-   print "Player 1 wins because paper disproves Spock! "
-elif answer1 == 3 and answer2 == 1:
-   print "Player 1 wins because rock crushes scissors! "
-elif answer1 == 3 and answer2 == 2:
-   print "Player 1 wins because scissors cuts paper! "
-elif answer1 == 3 and answer2 == 4:
-   print "Player 1 wins because scissors decapitate lizard! "
-elif answer1 == 3 and answer2 == 5:
-   print "Player 2 wins because Spock smashes scissors! "
-elif answer1 == 4 and answer2 == 1:
-   print "Player 2 wins because rock crushes lizard! "
-elif answer1 == 4 and answer2 == 2:
-   print "Player 1 wins because lizard eats paper! "
-elif answer1 == 4 and answer2 == 3:
-   print "Player 3 wins because scissors decapitate lizard! "
-elif answer1 == 4 and answer2 == 5:
-   print "Player 1 wins because lizard poisons Spock! "
-elif answer1 == 5 and answer2 == 1:
-   print "Player 1 wins because Spock vaporizes rock! "
-elif answer1 == 5 and answer2 == 2:
-   print "Player 2 wins because paper disproves Spock! "
-elif answer1 == 5 and answer2 == 3:
-   print "Player 1 wins because Spock crushes scissors! "
-elif answer1 == 5 and answer2 == 4:
-   print "Player 2 wins because lizard poisons Spock! "
+while answer1 > 5 or answer1 < 1:
+   answer1 = input ("Player 1 please type 1 for rock, 2 for paper, 3 for scissors, 4 for lizard, 5 for Spock ")
+
+while answer2 > 5 or answer2 < 1:
+   answer2 = input ("Player 2 please type 1 for rock, 2 for paper,  3 for scissors, 4 for lizard, 5 for Spock ")
+   
+   if answer1 == 1 and answer2 == 1:
+      print "Player 1 and 2 tie! "
+   elif answer1 == 1 and answer2 == 2:
+      print "Player 2 wins because paper covers rock! "
+   elif answer1 == 1 and answer2 == 3:
+      print "Player 1 wins because rock crushes scissors! " 
+   elif answer1 == 1 and answer2 == 4:
+      print "Player 1 wins because rock crushes lizard! "
+   elif answer1 == 1 and answer2 == 5:
+      print "Player 2 wins becuase Spock vaporizes rock! "
+   elif answer1 == 2 and answer2 == 1:
+      print "Player 2 wins because paper covers rock! "
+   elif answer1 == 2 and answer2 == 2:
+      print "Player 1 and 2 tie! "
+   elif answer1 == 2 and answer2 == 3:
+      print "Player 2 wins because scissors cuts paper! "
+   elif answer1 == 2 and answer2 == 4:
+      print "Player 2 wins because lizard eats paper! "
+   elif answer1 == 2 and answer2 == 5:
+      print "Player 1 wins because paper disproves Spock! "
+   elif answer1 == 3 and answer2 == 1:
+      print "Player 1 wins because rock crushes scissors! "
+   elif answer1 == 3 and answer2 == 2:
+      print "Player 1 wins because scissors cuts paper! "
+   elif answer1 == 3 and answer2 == 3:
+      print "Player 1 and 2 tie! "
+   elif answer1 == 3 and answer2 == 4:
+      print "Player 1 wins because scissors decapitate lizard! "
+   elif answer1 == 3 and answer2 == 5:
+      print "Player 2 wins because Spock smashes scissors! "
+   elif answer1 == 4 and answer2 == 1:
+      print "Player 2 wins because rock crushes lizard! "
+   elif answer1 == 4 and answer2 == 2:
+      print "Player 1 wins because lizard eats paper! "
+   elif answer1 == 4 and answer2 == 3:
+      print "Player 3 wins because scissors decapitate lizard! "
+   elif answer1 == 4 and answer2 == 2:
+      print "Player 1 and 2 tie! "
+   elif answer1 == 4 and answer2 == 5:
+      print "Player 1 wins because lizard poisons Spock! "
+   elif answer1 == 5 and answer2 == 1:
+      print "Player 1 wins because Spock vaporizes rock! "
+   elif answer1 == 5 and answer2 == 2:
+      print "Player 2 wins because paper disproves Spock! "
+   elif answer1 == 5 and answer2 == 3:
+      print "Player 1 wins because Spock crushes scissors! "
+   elif answer1 == 5 and answer2 == 4:
+      print "Player 2 wins because lizard poisons Spock! "
+   elif answer1 == 5 and answer2 == 5:
+      print "Player 1 and 2 tie! "
