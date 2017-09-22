fbf4181c8145d4119fe94df192711ac1ffcb7b98
Tim Blankenship <tim.blakenship@hp.com>
2017-09-22 15:30:50 -0700

    update script

--- ps1/test.ps1
+++ ps1/test.ps1
@@ -2,7 +2,13 @@
 
 $url = "http://adict.bcn.rd.hpicorp.net:88/cim/fmw/ipg-atlas_bcd/vulcan/wrl60-haswell_x86_64-dbg/180973/vulcan-trunk-R180973-170921.sh"
 $output = "$PSScriptRoot\vulcan-trunk-R180973-170921.sh" # $PSScriptRoot indicates the directory from which the script module is executed
-$start_time = Get-Date #Get-Date specifies the current date (or a date you define) 
+#$start_time = Get-Date #Get-Date specifies the current date (or a date you define) 
+
+## Gets the current date
+$start_time = Get-Date
+
+## Saves the current date and formats it (eg. 22-sep-2017 02:28)
+$DateStr = $start_time.ToString("dd-MMM-yyyy hh:mm")
 
 ## Grabs the web page and downloads the file
 #Invoke-WebRequest -Uri $url -OutFile $output
@@ -20,6 +26,6 @@ $WebResponse = Invoke-WebRequest http://adict.bcn.rd.hpicorp.net:88/cim/fmw/ipg-
 #$webResponse.tostring() -split "['`"'/]" | select-string "180973"
 
 ## Displays underlying HTML for select string (one line) 
-$webResponse.tostring() -split "['`"'/]" | select-string "180973" | Select -First 1 
+#$webResponse.tostring() -split "['`"'/]" | select-string "180973" | Select -First 1 
 
-#Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+Write-Output $DateStr # Prints the date in the requested format
