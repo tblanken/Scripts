625daa135f9ac26c62c10f142e9f0df09a03b96b
Tim Blankenship <tim.blakenship@hp.com>
2017-09-28 15:10:42 -0700

    update dl_file_from_url_with_timer.py

--- py/dl_file_from_url_with_timer.py
+++ py/dl_file_from_url_with_timer.py
@@ -1,25 +1,30 @@
 ##!/usr/bin/python
 
-# This script looks for the most recent directory found in the C:\temp directory and copies all .txt files
-# to the C:\temp2 directory
+# This script downloads firmware files at a specified time using a timer
 
 import os, datetime, shutil, sys, time, urllib.request 
 import profile as profile
 
-path = "c:\\temp\\"
-dest_path = "c:\\temp2\\" # This code defines the destination path of downloaded file
-dirs = filter(os.path.isdir, os.listdir ( path )) # Saves a list of the directories in the defined directory in a variable
+from datetime import datetime
+from threading import Timer
 
-file_list = [] # Creating a list to hold a list of files
-for i in dirs: # Creating a for loop to go through each file in the path
-    a = os.stat(os.path.join(path,i)) #os.stat returns file attributes about an inode. os.path.join joins one or more path components
-    file_list.append([time.ctime(a.st_ctime)]) #time of most recent metadata change on Unix, or the time of creation on Windows)
+x=datetime.today()
+y=x.replace(day=x.day+10, hour=11, minute=1, second=0, microsecond=0) # Set when to start executing the script
 
-newpath = (os.path.join(path,i)) # Defining the new path
+delta_t=y-x
+secs=delta_t.seconds+1
 
-for root, dirs, files in os.walk(newpath):
-    for y in files: # Creating a for loop to go through each file in the path
-        if y.endswith('.txt'): # Checking to see if file ends with .fmw
-##            print(os.path.join(newpath+ '\\' + y)) # For testing only
-            shutil.copy(newpath+ '\\' + y, dest_path) # Copies the files (noted as x) in the path to the destination 
+def download_files():
+    url_path = "http://adict.bcn.rd.hpicorp.net:88/cim/fmw/ipg-atlas_bcd/vulcan/wrl60-haswell_x86_64-dbg"
+    fw_folder = "180442"
+    fmw_file = "vulcan-trunk-R180442-170919.unsigned.fmw"
+    sh_file = "vulcan-trunk-R180442-170919.sh"
 
+#    get_fmw_file = urllib.request.URLopener()
+#    get_fmw_file.retrieve(url_path+ '/' + '/'+ fw_folder+ '/' + fmw_file, fmw_file)
+
+    getShFile = urllib.request.URLopener()
+    getShFile.retrieve(url_path+ '/' + '/'+ fw_folder+ '/' + sh_file, sh_file)
+
+t = Timer(secs, download_files)
+t.start() # Start the script
